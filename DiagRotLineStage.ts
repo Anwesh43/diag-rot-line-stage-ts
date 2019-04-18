cadabe3cdaf810d9d771c8899faae2eaa8b05075
Anwesh43 <anweshthecool0@gmail.com>
2019-04-18 17:04:11 +0530

    adding DiagRotLIneStage class

---
+++ DiagRotLineStage.ts
@@ -0,0 +1,41 @@
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const nodes : number = 5
+const lines : number = 4
+const foreColor : string = "green"
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+
+class DiagRotLineStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : DiagRotLineStage = new DiagRotLineStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
