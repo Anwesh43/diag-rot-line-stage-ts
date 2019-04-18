4f4ca90d3d47acf7d6c6842fb5af685195e23d13
Anwesh43 <anweshthecool0@gmail.com>
2019-04-18 17:49:33 +0530

    adding DrawingUtil class

--- DiagRotLineStage.ts
+++ DiagRotLineStage.ts
@@ -29,7 +29,43 @@ class ScaleUtil {
     }
 
     static updateValue(scale : number, dir : number, a : number, b : number) : number {
-        return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap 
+        return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap
+    }
+}
+
+class DrawingUtil {
+
+    static drawDiagRotLine(context : CanvasRenderingContext2D, deg : number, size : number, sc : number) {
+          context.save()
+          context.rotate(deg)
+          context.beginPath()
+          context.moveTo(0, 0)
+          context.lineTo(size * Math.sqrt(2), size * Math.sqrt(2))
+          context.stroke()
+          context.beginPath()
+          context.moveTo(size, size)
+          context.lineTo(size - 2 * size * sc, size)
+          context.stroke()
+          context.restore()
+    }
+
+    static drawDRLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const gap : number = w / (nodes + 1)
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, 2)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, 2)
+        const size : number = gap / sizeFactor
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = foreColor
+        context.save()
+        context.translate(gap * (i + 1), h / 2)
+        var deg = 0
+        for (var j = 0; j < lines; j++) {
+            const sc : number = ScaleUtil.divideScale(sc1, j, lines)
+            deg += (Math.PI / 2) * sc
+            DrawingUtil.drawDiagRotLine(context, deg, size, sc)
+        }
+        context.restore()
     }
 }
 
