0e5c76c763877d6fd6989d70fd92fb6ec6e68915
Anwesh43 <anweshthecool0@gmail.com>
2019-04-18 18:01:28 +0530

    adding DRLNode class

--- DiagRotLineStage.ts
+++ DiagRotLineStage.ts
@@ -142,3 +142,48 @@ class Animator {
         }
     }
 }
+
+class DRLNode {
+
+    prev : DRLNode
+    next : DRLNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new DRLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawDRLNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : DRLNode {
+        var curr : DRLNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
